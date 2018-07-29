d6de67438985689707c012c875838d714afab643
mike107 <mike_byun@hotmail.com>
2018-07-28 22:04:46 -0400

    Adding begining of block breaker game
    the lvl 1 does not transfer to lvl 2 after completion. No score. Working on lvl 2 atm, with sound.

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    
+    // config parameters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xLaunch = 2f;
+    [SerializeField] float yLaunch = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+    
+    // state
+    Vector2 paddleToBallVector;
+    bool Started = false;
+
+    // Cached Component References
+    AudioSource myAudioSource;
+
+    // Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!Started)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Started = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xLaunch, yLaunch);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Started)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+    }
+}
