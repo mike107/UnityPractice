cbad75ac2bfe3eefcefb89f90fcea7876a82323f
mike107 <mike_byun@hotmail.com>
2018-07-29 22:19:39 -0400

    build settings and scenes properly go to next level when the level is finished

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -6,10 +6,24 @@ public class Block : MonoBehaviour {
 
     [SerializeField] AudioClip breakSound;
 
+    //cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountbreakableBlocks();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestoryBlock();
+    }
+
+    private void DestoryBlock()
+    {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
